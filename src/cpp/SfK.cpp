9caa8dbb3126c7e96a48227a20e8e7452d65eff2
franz lübke <toxxxsys@gmail.com>
2015-05-09 17:21:13 +0200

    Klasse Angelegt

---
+++ src/cpp/SfK.cpp
@@ -0,0 +1,18 @@
+/*
+ * SfK.cpp
+ *
+ *  Created on: 09.05.2015
+ *      Author: franz
+ */
+
+#include "../h/SfK.h"
+
+SfK::SfK() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SfK::~SfK() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/h/SfK.h
@@ -0,0 +1,19 @@
+/*
+ * SfK.h
+ *
+ *  Created on: 09.05.2015
+ *      Author: franz
+ */
+
+#ifndef SRC_SFK_H_
+#define SRC_SFK_H_
+
+#include "Strategie.h"
+
+class SfK: public Strategie {
+public:
+	SfK();
+	virtual ~SfK();
+};
+
+#endif /* SRC_SFK_H_ */
