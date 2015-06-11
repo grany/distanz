cd9a79d4b77d63a0d50992e16a6cae3442bfcc0c
MadMadd <martin.schleitzer@t-online.de>
2015-06-11 17:53:24 +0100

    Rechtschreibung mal geprüft

--- src/h/Strategie.h
+++ src/h/Strategie.h
@@ -17,7 +17,7 @@
 /*
  * class Strategie
  *
- * Abstrakte klasse zur Erzeugung von Spetziellen zug Strategien
+ * Abstrakte Klasse zur Erzeugung von speziellen Zug-Strategien
  */
 class Strategie {
 protected:
@@ -26,9 +26,9 @@ protected:
 	SpielBrett &brett;
 
 	int wert;
-	std::vector<zug> mZuege; 	//Mögliche Züge
-	std::vector<zug> aZuege; 	//Auswahl der Zuege nach priorietät sortiert
-	zug nZug;					// Nächster zug
+	std::vector<zug> mZuege; 	//MÃ¶gliche ZÃ¼ge
+	std::vector<zug> aZuege; 	//Auswahl der Zuege nach PrioritÃ¤t sortiert
+	zug nZug;					// NÃ¤chster zug
 
 	void getmZuege(std::vector<zug> &zuege);
 	virtual void bewerten()=0;
