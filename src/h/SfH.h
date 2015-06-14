32612c158afafa6ea29489a0392c8576aefea34e
MadMadd <martin.schleitzer@t-online.de>
2015-06-14 21:18:12 +0100

    Umlaute

--- src/h/SfH.h
+++ src/h/SfH.h
@@ -13,7 +13,7 @@
 /**
  * class SfH (Strategie fange Helfer)
  * Ist eine Ableitung der abstrakten Klasse Strategie.
- * Überschreibt/implementiert die Methode bewerten();
+ * Ueberschreibt/implementiert die Methode bewerten();
  * @param &team Referenz auf Instanz von Team
  * @param &b 	Referenz auf Instanz von SpielBrett
  */
@@ -21,7 +21,7 @@ class SfH: public Strategie {
 public:
 	/*
 	 * bewerten()
-	 * Bewertet mögliche Züge nach der Möglichkeit gegnerische Helfer zu fangen.
+	 * Bewertet moegliche Zuege nach der Moeglichkeit gegnerische Helfer zu fangen.
 	 */
 	virtual void bewerten() override;
 	SfH(Team &team, SpielBrett &b);
